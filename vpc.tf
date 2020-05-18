30fbdb2a70128f32d4d5fea943022f01bbb852fa
Yegor Ius <yegor.ius@enigma.com>
2020-05-17 23:37:59 -0400

    Make VPC availability zones unique

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,3 @@
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "1.66.0"
@@ -9,7 +5,7 @@ module "vpc" {
   name = "Main"
   cidr = "10.0.0.0/16"
 
-  azs = "${ slice( data.aws_availability_zones.available.names, 1, var.vpc_number_of_azs ) }"
+  azs = ["${var.region}a", "${var.region}b", "${var.region}c"]
 
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
